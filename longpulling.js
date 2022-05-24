c2ee60d51950aeb7350ec6d910889c705af7e233
Roman <romanahramenya@gmail.com>
2022-05-24 06:07:08 +0300

    package

--- longpulling.js
+++ longpulling.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const events = require('events')
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const emmitter = new events.EventEmitter()
 const app = express()
